27598b5c0df236ab016a775ea45627a6710128dc
Manuel Arostegui <marostegui@wikimedia.org>
2022-01-11 07:11:52 +0100

    drop_rev_page_id_T285149.py: Schema change
    Dropping rev_page_id index
    
    Bug: T285149
    Change-Id: I55f9a13d55fac936b1eca433dbcf74e299bf68c4

---
+++ 2022/drop_rev_page_id_T285149.py
@@ -0,0 +1,37 @@
+from auto_schema.schema_change import SchemaChange
+
+# Copy this file and make adjustments
+
+# Set to None or 0 to skip downtiming
+downtime_hours = 6
+ticket = 'T285149'
+
+# Don't add set session sql_log_bin=0;
+command = """alter table revision drop key if exists rev_page_id;"""
+
+# Set this to false if you don't want to run on all dbs
+# In that case, you have to specify the db in the command and check function.
+all_dbs = True
+
+# DO NOT FORGET to set the right port if it's not 3306
+# Use None instead of [] to get all direct replicas of master of active dc
+replicas = ['db1098:3316']
+section = 's6'
+
+# The check function must return true if schema change is applied
+# or not needed, False otherwise.
+
+
+def check(db):
+    return 'rev_page_id' not in db.run_sql('show indexes from revision;')
+
+schema_change = SchemaChange(
+    replicas=replicas,
+    section=section,
+    all_dbs=all_dbs,
+    check=check,
+    command=command,
+    ticket=ticket,
+    downtime_hours=downtime_hours
+)
+schema_change.run()
