e3be641260d15b986442443a6f75513d609fc086
Manuel Arostegui <marostegui@wikimedia.org>
2022-01-25 09:50:51 +0100

    add_gb_by_central_id_T299827.py: Fixes
    I think the previous commit used an old structure of the
    run_schema_change.py
    
    Bug: T299827
    Change-Id: I84bbea3dd11976b8ca9bb6d34fe15c7e36aca590

--- 2022/add_gb_by_central_id_T299827.py
+++ 2022/add_gb_by_central_id_T299827.py
@@ -2,31 +2,29 @@ from auto_schema.schema_change import SchemaChange
 
 # Copy this file and make adjustments
 
-section = 's7'
-should_depool = 'auto'
+# Set to None or 0 to skip downtiming
 downtime_hours = 6
-should_downtime = True
 ticket = 'T299827'
 
 # Don't add set session sql_log_bin=0;
 command = """use centralauth;ALTER TABLE globalblocks ADD  gb_by_central_id INT UNSIGNED DEFAULT NULL;"""
 
 # Set this to false if you don't want to run on all dbs
-# In that case, you have to specify the db in the command.
-all_dbs = False
+# In that case, you have to specify the db in the command and check function.
+all_dbs = False 
 
 # DO NOT FORGET to set the right port if it's not 3306
-# Use None instead of [] to get all pooled replicas
+# Use None instead of [] to get all direct replicas of master of active dc
 replicas = None
+section = 's7'
 
-# Should return true if schema change is applied
-
+# The check function must return true if schema change is applied
+# or not needed, False otherwise.
 
 def check(db):
     if 'globaluser' not in db.run_sql('use centralauth;show tables;'):
         return True
     return 'gb_by_central_id' in db.run_sql('use centralauth;desc globalblocks;')
-
 
 schema_change = SchemaChange(
     replicas=replicas,
@@ -35,7 +33,6 @@ schema_change = SchemaChange(
     check=check,
     command=command,
     ticket=ticket,
-    downtime_hours=downtime_hours,
-    should_depool=should_depool
+    downtime_hours=downtime_hours
 )
 schema_change.run()
