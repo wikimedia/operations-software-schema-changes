820d5b89e65e26284179bf96c4e806f9095b869d
Manuel Arostegui <marostegui@wikimedia.org>
2022-02-14 12:51:19 +0100

    change_pp_page_T300381.py: New schema change
    Bug: T300381
    Change-Id: Id4a9cc6cc9b374426622799e4e741b60eabacb24

---
+++ 2022/change_pp_page_T300381.py
@@ -0,0 +1,41 @@
+from auto_schema.schema_change import SchemaChange
+
+# Copy this file and make adjustments
+
+# Set to None or 0 to skip downtiming
+downtime_hours = 6
+ticket = 'T300381'
+
+# Don't add set session sql_log_bin=0;
+command = """ALTER TABLE  page_props CHANGE  pp_page pp_page INT UNSIGNED NOT NULL;"""
+
+# Set this to false if you don't want to run on all dbs
+# In that case, you have to specify the db in the command and check function.
+all_dbs = True
+
+# DO NOT FORGET to set the right port if it's not 3306
+# Use None instead of [] to get all direct replicas of master of active dc
+replicas = None
+section = 's6'
+
+# The check function must return true if schema change is applied
+# or not needed, False otherwise.
+
+def check(db):
+    query_res = db.run_sql('desc page_props;')
+    if not query_res:
+        # Dry run
+        return True
+    field_def = query_res.split('pp_page')[1].split('\n')[0]
+    return 'unsigned' in field_def.lower()
+
+schema_change = SchemaChange(
+    replicas=replicas,
+    section=section,
+    all_dbs=all_dbs,
+    check=check,
+    command=command,
+    ticket=ticket,
+    downtime_hours=downtime_hours
+)
+schema_change.run()
