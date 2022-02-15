895839cfdf30f8d2c3b3c48c636bb1f916e98378
Stevie Beth Mhaol <sbmhaol@wikimedia.org>
2022-02-15 11:22:34 +0100

    Add 2022/drop_fr_img_star_cols_T300774.py
    Bug: T300774
    Change-Id: I88d6aee61674304669379c380cebeaebbd59a8f6

---
+++ 2022/drop_fr_img_star_cols_T300774.py
@@ -0,0 +1,37 @@
+from auto_schema.schema_change import SchemaChange
+
+# Set to None or 0 to skip downtiming
+downtime_hours = 6
+ticket = 'T300774'
+
+# Don't add set session sql_log_bin=0;
+command = """DROP INDEX fr_img_sha1 ON /*_*/flaggedrevs; ALTER TABLE /*_*/flaggedrevs DROP COLUMN fr_img_name, DROP COLUMN fr_img_timestamp, DROP COLUMN fr_img_sha1;"""
+
+# Set this to false if you don't want to run on all dbs
+# In that case, you have to specify the db in the command and check function.
+all_dbs = True
+
+# DO NOT FORGET to set the right port if it's not 3306
+# Use None instead of [] to get all direct replicas of master of active dc
+replicas = None
+section = 's5'
+
+# The check function must return true if schema change is applied
+# or not needed, False otherwise.
+
+def check(db):
+    if 'flaggedrevs' not in db.run_sql('show tables;'):
+        return True
+    query_res = db.run_sql('desc flaggedrevs;')
+    return "fr_img_sha1" in query_res.split()
+
+schema_change = SchemaChange(
+    replicas=replicas,
+    section=section,
+    all_dbs=all_dbs,
+    check=check,
+    command=command,
+    ticket=ticket,
+    downtime_hours=downtime_hours
+)
+schema_change.run()
