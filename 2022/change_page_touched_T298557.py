70b83d5903e8b2c0191ec5eb5d5b5d0fd2f4c61e
Manuel Arostegui <marostegui@wikimedia.org>
2022-03-15 07:30:29 +0100

    change_page_touched_T298557.py: New schema change
    Bug: T298557
    Change-Id: I6eb48a58a3b24b9befee4190d7c3e2af35eec55a

---
+++ 2022/change_page_touched_T298557.py
@@ -0,0 +1,41 @@
+from auto_schema.schema_change import SchemaChange
+
+# Copy this file and make adjustments
+
+# Set to None or 0 to skip downtiming
+downtime_hours = 6
+ticket = 'T298557'
+
+# Don't add set session sql_log_bin=0;
+command = """ALTER TABLE page CHANGE page_touched page_touched BINARY(14) NOT NULL;"""
+
+# Set this to false if you don't want to run on all dbs
+# In that case, you have to specify the db in the command and check function.
+all_dbs = True
+
+# DO NOT FORGET to set the right port if it's not 3306
+# Use None instead of [] to get all direct replicas of master of active dc
+replicas = None
+section = 's6'
+
+# The check function must return true if schema change is applied
+# or not needed, False otherwise.
+
+def check(db):
+    query_res = db.run_sql('desc page;')
+    if not query_res:
+        # Dry run
+        return True
+    field_def = query_res.split('page_touched')[1].split('\n')[0]
+    return 'varbinary' not in field_def.lower()
+
+schema_change = SchemaChange(
+    replicas=replicas,
+    section=section,
+    all_dbs=all_dbs,
+    check=check,
+    command=command,
+    ticket=ticket,
+    downtime_hours=downtime_hours
+)
+schema_change.run()
