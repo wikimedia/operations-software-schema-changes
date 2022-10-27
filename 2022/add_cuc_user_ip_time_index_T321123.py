3a539057aea2c2296d48c494cdc87752a78253d5
Manuel Arostegui <marostegui@wikimedia.org>
2022-10-27 08:07:24 +0200

    add_cuc_user_ip_time_index_T321123.py: New schema change
    Bug: T321123
    Change-Id: I2c82c0e7f308e662743c27a8d95eb093ed11dfe8

---
+++ 2022/add_cuc_user_ip_time_index_T321123.py
@@ -0,0 +1,35 @@
+from auto_schema.schema_change import SchemaChange
+
+# Copy this file and make adjustments
+
+# Set to None or 0 to skip downtiming
+downtime_hours = 8
+ticket = 'T321123'
+
+# Don't add set session sql_log_bin=0;
+command = """ALTER TABLE cu_changes ADD INDEX IF NOT EXISTS cuc_user_ip_time (cuc_user,cuc_ip,cuc_timestamp), DROP INDEX cuc_user_time;"""
+
+# Set this to false if you don't want to run on all dbs
+# In that case, you have to specify the db in the command and check function.
+all_dbs = True
+
+# DO NOT FORGET to set the right port if it's not 3306
+# Use None instead of [] to get all direct replicas of master of active dc
+replicas = None
+section = 's6'
+
+# The check function must return true if schema change is applied
+# or not needed, False otherwise.
+def check(db):
+    return 'cuc_user_time' not in db.run_sql('show indexes from cu_changes;')
+
+schema_change = SchemaChange(
+    replicas=replicas,
+    section=section,
+    all_dbs=all_dbs,
+    check=check,
+    command=command,
+    ticket=ticket,
+    downtime_hours=downtime_hours
+)
+schema_change.run()
