b0146684fcad6c0371026a165c972a48872b9d67
Manuel Arostegui <marostegui@wikimedia.org>
2023-01-04 12:45:50 +0100

    change_cul_user_T326011.py: New schema change
    Bug: T326011
    Change-Id: I5e086eed16b80038165b57893f88cf570724b97c

---
+++ 2022/change_cul_user_T326011.py
@@ -0,0 +1,41 @@
+from auto_schema.schema_change import SchemaChange
+import re
+# Copy this file and make adjustments
+
+# Set to None or 0 to skip downtiming
+downtime_hours = 4
+ticket = 'T326011'
+
+# Don't add set session sql_log_bin=0;
+command = """ALTER TABLE cu_log CHANGE  cul_user cul_user INT UNSIGNED DEFAULT 0 NOT NULL, CHANGE  cul_user_text cul_user_text VARBINARY(255) DEFAULT '' NOT NULL;"""
+
+# Set this to false if you don't want to run on all dbs
+# In that case, you have to specify the db in the command and check function.
+all_dbs = True
+
+# DO NOT FORGET to set the right port if it's not 3306
+# Use None instead of [] to get all direct replicas of master of active dc
+replicas = None
+section = 's3'
+
+# The check function must return true if schema change is applied
+# or not needed, False otherwise.
+
+def check(db):
+    query_res = db.run_sql('desc cu_log;')
+    if not query_res:
+        # Dry run
+        return True
+    field_def = query_res.split('cul_user')[1].split('\n')[0]
+    return bool(re.findall(r'\b0\b', field_def.lower()))
+
+schema_change = SchemaChange(
+    replicas=replicas,
+    section=section,
+    all_dbs=all_dbs,
+    check=check,
+    command=command,
+    ticket=ticket,
+    downtime_hours=downtime_hours
+)
+schema_change.run()
