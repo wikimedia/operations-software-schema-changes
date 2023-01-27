fa5bf4bf927d5479bb608dc7488566e2721f342c
Manuel Arostegui <marostegui@wikimedia.org>
2023-01-27 09:20:08 +0100

    drop_cul_user_text_T328086.py: New schema change
    Bug: T328086
    Change-Id: I886497a527067e9cbbf2656ad491bad47a0478bb

---
+++ 2023/drop_cul_user_text_T328086.py
@@ -0,0 +1,36 @@
+from auto_schema.schema_change import SchemaChange
+
+# Copy this file and make adjustments
+
+# Set to None or 0 to skip downtiming
+downtime_hours = 6
+ticket = 'T328086'
+
+# Don't add set session sql_log_bin=0;
+command = """DROP  INDEX cul_user ON cu_log; ALTER TABLE cu_log DROP  cul_user, DROP  cul_user_text;"""
+
+# Set this to false if you don't want to run on all dbs
+# In that case, you have to specify the db in the command and check function.
+all_dbs = False
+
+# DO NOT FORGET to set the right port if it's not 3306
+# Use None instead of [] to get all direct replicas of master of active dc
+replicas = None
+section = 's6'
+
+# The check function must return true if schema change is applied
+# or not needed, False otherwise.
+
+def check(db):
+    return 'cul_user' not in db.run_sql('show index from cu_log;')
+
+schema_change = SchemaChange(
+    replicas=replicas,
+    section=section,
+    all_dbs=all_dbs,
+    check=check,
+    command=command,
+    ticket=ticket,
+    downtime_hours=downtime_hours
+)
+schema_change.run()
