a107f2127c61dd532672754d99252f0f692eb3f7
Amir Sarabadani <ladsgroup@gmail.com>
2023-08-07 16:17:07 +0200

    Drop old externallinks columns
    Bug: T343718
    Change-Id: Ifa9239cefd00eb18e47f3176947641ffc0588343

---
+++ 2023/drop_el_to_T343718.py
@@ -0,0 +1,43 @@
+from auto_schema.schema_change import SchemaChange
+
+# Copy this file and make adjustments
+
+# Set to None or 0 to skip downtiming
+downtime_hours = 24
+ticket = 'T343718'
+
+# Don't add set session sql_log_bin=0;
+command = """ALTER TABLE externallinks
+DROP COLUMN el_to,
+DROP COLUMN el_index,
+DROP COLUMN el_index_60,
+DROP INDEX el_to,
+DROP INDEX el_index,
+DROP INDEX el_index_60,
+DROP INDEX el_from_index_60;"""
+
+# Set this to false if you don't want to run on all dbs
+# In that case, you have to specify the db in the command and check function.
+all_dbs = True
+
+# DO NOT FORGET to set the right port if it's not 3306
+# Use None instead of [] to get all direct replicas of master of active dc
+replicas = None
+section = 's6'
+
+# The check function must return true if schema change is applied
+# or not needed, False otherwise.
+
+def check(db):
+    return 'el_to' not in db.get_columns('externallinks')
+
+schema_change = SchemaChange(
+    replicas=replicas,
+    section=section,
+    all_dbs=all_dbs,
+    check=check,
+    command=command,
+    ticket=ticket,
+    downtime_hours=downtime_hours
+)
+schema_change.run()
